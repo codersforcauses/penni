ec73ca73cfb0265ca7c9678cff780da114b9e041
dct0 <github-action@actions-template-sync.noreply.github.com>
2024-06-21 05:51:58 +0000

    chore(template): merge template changes :up:
    Signed-off-by: dct0 <github-action@actions-template-sync.noreply.github.com>

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,23 @@
+import os from "node:os";
+
+import isInsideContainer from "is-inside-container";
+
+const isWindowsDevContainer = () =>
+  os.release().toLowerCase().includes("microsoft") && isInsideContainer();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // dumb fix for windows docker
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    };
-
-    return config;
-  },
+  webpack: isWindowsDevContainer()
+    ? (config) => {
+        config.watchOptions = {
+          poll: 1000,
+          aggregateTimeout: 300,
+        };
+        return config;
+      }
+    : undefined,
 };
 
 export default nextConfig;
