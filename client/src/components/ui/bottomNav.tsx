956cdf5ec4ccbbc63e6661b9743e3b1e81515dee
ErikaKK <491649804@qq.com>
2024-06-30 00:46:49 +0800

    Add index in map method

--- client/src/components/ui/bottomNav.tsx
+++ client/src/components/ui/bottomNav.tsx
@@ -19,13 +19,21 @@ const iconLists = [
 ];
 
 export default function BottomNav() {
-  const iconItems = iconLists.map((iconItem) => (
-    <div className="flex w-1/3 flex-col items-center">
-      <div>
-        <Image src={iconItem.ref} alt={iconItem.alt} width={24} height={24} />
-      </div>
-      <p>{iconItem.text}</p>
+  return (
+    <div className="flex h-20 pt-2 text-xs leading-3">
+      {iconLists.map((iconItem, index) => (
+        <div key={index} className="flex w-1/3 flex-col items-center">
+          <div>
+            <Image
+              src={iconItem.ref}
+              alt={iconItem.alt}
+              width={24}
+              height={24}
+            />
+          </div>
+          <p>{iconItem.text}</p>
+        </div>
+      ))}
     </div>
-  ));
-  return <div className="flex h-20 pt-2 text-xs leading-3">{iconItems}</div>;
+  );
 }
