023ad2089c68dc5f2e0f6b6303af9b59fc7c4b57
ErikaKK <491649804@qq.com>
2024-06-30 16:25:27 +0800

    Add svg icon component. Change icon color and layout size to become the same as Figma design

--- client/src/components/ui/bottomNav.tsx
+++ client/src/components/ui/bottomNav.tsx
@@ -1,18 +1,97 @@
-import Image from "next/image";
+import * as React from "react";
+
+interface BottomNavIconProps extends React.SVGProps<SVGSVGElement> {
+  className?: string;
+  alt?: string;
+}
+
+const BottomNavIcon1: React.FC<BottomNavIconProps> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M8 6h13M8 12h13M8 18h13M3 6h.01M3 12h.01M3 18h.01"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+const BottomNavIcon2: React.FC<BottomNavIconProps> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M10 3H3v7h7V3zM21 3h-7v7h7V3zM21 14h-7v7h7v-7zM10 14H3v7h7v-7z"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+const BottomNavIcon3: React.FC<BottomNavIconProps> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2M12 11a4 4 0 100-8 4 4 0 000 8z"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export { BottomNavIcon1, BottomNavIcon2, BottomNavIcon3 };
 
 const iconLists = [
   {
-    ref: "/img/bottomNavIcon1.svg",
+    IconComponent: BottomNavIcon1,
     alt: "Icon of my tasks",
     text: "My Tasks",
   },
   {
-    ref: "/img/bottomNavIcon2.svg",
+    IconComponent: BottomNavIcon2,
     alt: "Icon of market",
     text: "Market",
   },
   {
-    ref: "/img/bottomNavIcon3.svg",
+    IconComponent: BottomNavIcon3,
     alt: "Icon of me",
     text: "Me",
   },
@@ -20,20 +99,23 @@ const iconLists = [
 
 export default function BottomNav() {
   return (
-    <div className="flex h-20 pt-2 text-xs leading-3">
-      {iconLists.map((iconItem, index) => (
-        <div key={index} className="flex w-1/3 flex-col items-center">
-          <div>
-            <Image
-              src={iconItem.ref}
-              alt={iconItem.alt}
-              width={24}
-              height={24}
-            />
+    <div className="border-t-penni-border-light-mode h-[83px] border-t-2">
+      <div className="flex h-[49px] cursor-pointer text-[10px] leading-3">
+        {iconLists.map((iconItem, index) => (
+          <div
+            key={index}
+            className="flex w-1/3 flex-col items-center pt-[6px] text-penni-text-tertiary-light-mode"
+          >
+            <div>
+              <iconItem.IconComponent
+                className="text-penni-text-tertiary-light-mode"
+                alt={iconItem.alt}
+              />
+            </div>
+            <p>{iconItem.text}</p>
           </div>
-          <p>{iconItem.text}</p>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
