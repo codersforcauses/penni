cfbdd3125939a634e944b58511508eb9371cb9c5
dct0 <github-action@actions-template-sync.noreply.github.com>
2024-06-30 00:11:31 +0000

    chore(template): merge template changes :up:
    Signed-off-by: dct0 <github-action@actions-template-sync.noreply.github.com>

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { Inter as FontSans } from "next/font/google";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { usePings } from "@/hooks/pings";
 import { cn } from "@/lib/utils";
 
 import { Button } from "../components/ui/button";
-import SplashScreen from "../components/ui/SplashScreen";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -14,21 +13,9 @@ const fontSans = FontSans({
 
 export default function Home() {
   const [clicked, setClicked] = useState(false);
-  const [showSplash, setShowSplash] = useState(true);
   const { data, isLoading } = usePings({
     enabled: clicked,
   });
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSplash(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (showSplash) {
-    return <SplashScreen />;
-  }
 
   return (
     <main
