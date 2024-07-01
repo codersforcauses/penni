2de9c0ca8a7b8479c072af91d4513c9f7af636cb
SodaVolcano <23301921@student.uwa.edu.au>
2024-07-01 08:29:14 +0000

    input styling

--- client/src/components/field.tsx
+++ client/src/components/field.tsx
@@ -24,19 +24,42 @@ interface InputProp {
   description?: string;
 }
 
+// Using tailwind 'jit" so can't use string concatenation
+const labelNoDescription =
+  "m-4 flex h-14 w-full flex-col rounded-penni-border bg-black bg-opacity-5 px-4 pb-2 pt-3";
+const labelWithDescription =
+  "m-4 flex h-14 w-full flex-col rounded-penni-border bg-black bg-opacity-5 py-4 pl-4 pr-3";
+
+function InputDescription({ description }: { description: string }) {
+  return;
+  <span className="w-full text-xs font-normal leading-3 text-penni-text-secondary-light-mode">
+    Input
+  </span>;
+}
+
 export function SingleLineInput({ value, onChange, description }: InputProp) {
   return (
-    <div className="m-4 h-14 w-full rounded-penni-border bg-black bg-opacity-5 py-4 pl-4 pr-3">
-      <label>
-        <span>Input</span>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-          className="w-full bg-penni-text-regular-light-mode bg-transparent text-2xl"
-        />
-      </label>
-    </div>
+    <label className={labelNoDescription}>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        className="h-full w-full bg-transparent text-base font-normal leading-5 text-penni-text-regular-light-mode"
+      />
+    </label>
+  );
+}
+
+export function SingleLineInput2({ value, onChange, description }: InputProp) {
+  return (
+    <label className="m-4 flex h-14 w-full flex-col rounded-penni-border bg-black bg-opacity-5 py-4 pl-4 pr-3">
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        className="h-full w-full bg-transparent text-base font-normal leading-5 text-penni-text-regular-light-mode"
+      />
+    </label>
   );
 }
 /*
--- client/src/pages/test.tsx
+++ client/src/pages/test.tsx
@@ -2,8 +2,13 @@ import { SingleLineInput } from "@/components/field";
 
 export default function Test() {
   return (
-    <div className="m-32">
+    <>
       <SingleLineInput value="hello" onChange={() => {}} />
-    </div>
+      <SingleLineInput
+        value="adawd"
+        onChange={() => {}}
+        description="yo buddy"
+      />
+    </>
   );
 }
