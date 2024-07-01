af82499f44bd3006377be2777fd47c9ef30340da
SodaVolcano <23301921@student.uwa.edu.au>
2024-07-01 05:26:16 +0000

    edit icon

--- server/api/settings.py
+++ server/api/settings.py
@@ -139,8 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))  # <- '/' directory
+PROJECT_ROOT = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)  # <- '/' directory
 
 STATIC_URL = "/static/"
 
