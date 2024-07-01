90d03658d93ba92afaae5e9bf2bf77e550e67d8e
SodaVolcano <23301921@student.uwa.edu.au>
2024-07-01 13:32:51 +0000

    docstrings and finished input components

--- client/src/components/field.tsx
+++ client/src/components/field.tsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 
-// Using tailwind 'jit" so can't use string concatenation
-const inputStyleNoLabel =
-  "m-4 flex h-14 w-auto flex-col rounded-penni-border bg-black bg-opacity-5 py-4 pl-4 pr-3";
-const inputStyleWithLabel =
-  "m-4 flex h-14 w-auto flex-col rounded-penni-border bg-black bg-opacity-5 px-4 pb-2 pt-3";
+/*
+TODOS: 
+- style scroll bar in textarea?
+- fix priceInput to 2 decimals?
+*/
+
+// Tailwind 'jit' don't support str concatenation so duplicating styles here... lord forgive my sins
+const textStyleNoLabel =
+  "m-4 flex h-14 w-auto flex-col rounded-penni-border bg-black bg-opacity-5 py-4 pl-4 pr-3 overflow-hidden";
+const textStyleWithLabel =
+  "m-4 flex h-14 w-auto flex-col rounded-penni-border bg-black bg-opacity-5 px-4 pb-2 pt-3 overflow-hidden";
+const paragraphStyleNoLabel =
+  "m-4 flex h-auto w-auto flex-col rounded-penni-border bg-black bg-opacity-5 py-4 pl-4 pr-3 overflow-y-auto";
+const paragraphStyleWithLabel =
+  "m-4 flex h-auto w-auto flex-col rounded-penni-border bg-black bg-opacity-5 py-4 pl-4 pr-3 overflow-y-auto";
+
 // colour added to safelist so can concatenate colour in function
 const valueStyle =
-  "h-full w-full bg-transparent text-base font-normal leading-5 focus:outline-none overflow-hidden resize-none";
+  "h-full w-full bg-transparent text-base font-normal leading-5 focus:outline-none  resize-none";
 // Generate unique ID for each component, used for label htmlFor attribute
 const uniqueId = () => `${Date.now()}-${Math.random()}`;
 
@@ -20,7 +31,14 @@ interface InputProp {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLEventTargetElement>) => void;
   label?: string;
-  defaultValue?: string;
+}
+
+interface DropdownInputProp extends InputProp {
+  options: string[];
+}
+
+interface FreeTextInputProp extends InputProp {
+  placeholder?: string;
 }
 
 function InputLabel({ label, id }: { label: string; id: string }) {
@@ -34,11 +52,32 @@ function InputLabel({ label, id }: { label: string; id: string }) {
   );
 }
 
-function TextInput({ value, onChange, label }: InputProp) {
+/**
+ * Input component for text on a single line.
+ *
+ * @param props - The properties for the TextInput component.
+ * @param props.value - The current text value of the input, tracked by `react.setState()`.
+ * @param props.onChange - Called when the text value changes, should update `value`.
+ * @param props.label - (*Optional*) Label for the field, displayed above the input.
+ * @param props.placeholder - (*Optional*) Placeholder text for the input field.
+ *
+ * @returns Input component for free text.
+ *
+ * @example
+ * // Example usage:
+ * const [textValue, setTextValue] = useState('');
+ * <TextInput
+ *   value={textValue}
+ *   onChange={(e) => setTextValue(e.target.value)}
+ *   label="Enter text"
+ *   placeholder="Type here..."
+ * />
+ */
+function TextInput({ value, onChange, label, placeholder }: FreeTextInputProp) {
   const [valueChanged, setValueChanged] = useState(false); // show lighter grey if not changed
   const [id] = useState(uniqueId());
   return (
-    <div className={label ? inputStyleWithLabel : inputStyleNoLabel}>
+    <div className={label ? textStyleWithLabel : textStyleNoLabel}>
       {label && <InputLabel label={label} id={id} />}
       <input
         id={id}
@@ -48,6 +87,7 @@ function TextInput({ value, onChange, label }: InputProp) {
           onChange(e);
           setValueChanged(true);
         }}
+        placeholder={placeholder}
         className={
           valueStyle +
           (valueChanged
@@ -59,11 +99,37 @@ function TextInput({ value, onChange, label }: InputProp) {
   );
 }
 
-function PriceInput({ value, onChange, label }: InputProp) {
+/**
+ * Input for price values, with a dollar sign and two decimal places.
+ *
+ * @param props - The properties for the PriceInput component.
+ * @param props.value - The current numeric value of the input, tracked by `react.setState()`.
+ * @param props.onChange - Called when the numeric value changes, should update `value`.
+ * @param props.label - (*Optional*) Label for the field, displayed above the input.
+ * @param props.placeholder - (*Optional*) Placeholder text for the input field.
+ *
+ * @returns Input component for entering price values.
+ *
+ * @example
+ * // Example usage:
+ * const [priceValue, setPriceValue] = useState(0);
+ * <PriceInput
+ *   value={priceValue}
+ *   onChange={(e) => setPriceValue(parseFloat(e.target.value))}
+ *   label="Enter price"
+ *   placeholder="0.00"
+ * />
+ */
+function PriceInput({
+  value,
+  onChange,
+  label,
+  placeholder,
+}: FreeTextInputProp) {
   const [valueChanged, setValueChanged] = useState(false); // show lighter grey if not changed
   const [id] = useState(uniqueId());
   return (
-    <div className={label ? inputStyleWithLabel : inputStyleNoLabel}>
+    <div className={label ? textStyleWithLabel : textStyleNoLabel}>
       {label && <InputLabel label={label} id={id} />}
       <div className="item-center flex size-full flex-row">
         <span className="size-4">$</span>
@@ -71,6 +137,7 @@ function PriceInput({ value, onChange, label }: InputProp) {
           id={id}
           type="number"
           value={value}
+          placeholder={placeholder}
           onChange={(e) => {
             onChange(e);
             setValueChanged(true);
@@ -87,15 +154,43 @@ function PriceInput({ value, onChange, label }: InputProp) {
   );
 }
 
-function ParagraphInput({ value, onChange, label }: InputProp) {
+/**
+ * ParagraphInput Component
+ *
+ * @param props - The properties for the ParagraphInput component.
+ * @param props.value - The current text value of the textarea, tracked by `react.setState()`.
+ * @param props.onChange - Called when the text value changes, should update `value`.
+ * @param props.label - (*Optional*) Label for the field, displayed above the textarea.
+ * @param props.placeholder - (*Optional*) Placeholder text for the textarea.
+ *
+ * @returns Textarea component for entering paragraphs of text.
+ *
+ * @example
+ * // Example usage:
+ * const [paragraphValue, setParagraphValue] = useState('');
+ * <ParagraphInput
+ *   value={paragraphValue}
+ *   onChange={(e) => setParagraphValue(e.target.value)}
+ *   label="Enter text"
+ *   placeholder="Type here..."
+ * />
+ */
+function ParagraphInput({
+  value,
+  onChange,
+  label,
+  placeholder,
+}: FreeTextInputProp) {
   const [valueChanged, setValueChanged] = useState(false); // show lighter grey if not changed
   const [id] = useState(uniqueId());
   return (
-    <div className={label ? inputStyleWithLabel : inputStyleNoLabel}>
+    <div className={label ? paragraphStyleWithLabel : paragraphStyleNoLabel}>
       {label && <InputLabel label={label} id={id} />}
       <textarea
         id={id}
         value={value}
+        rows={5}
+        placeholder={placeholder}
         onChange={(e) => {
           onChange(e);
           setValueChanged(true);
@@ -111,15 +206,35 @@ function ParagraphInput({ value, onChange, label }: InputProp) {
   );
 }
 
-function DropdownInput({ value, onChange, label }: InputProp) {
+/**
+ * Input component with a list of options.
+ 
+ * @param props - The properties for the DropdownInput component.
+ * @param props.value - The current selected value of the dropdown, tracked by `react.setState()`
+ * @param props.options - List of possible values for `value`, displayed in dropdown.
+ * @param props.onChange - Called when the selected value changes, should update `value`.
+ * @param props.label - (*Optional*) Label for the field, displayed above input.
+ *
+ * @returns Input component with a list of dropdown options.
+ *
+ * @example
+ * // Example usage:
+ * const [selectedValue, setSelectedValue] = useState('');
+ * <DropdownInput
+ *   value={selectedValue}
+ *   options={['Option 1', 'Option 2', 'Option 3']}
+ *   onChange={(e) => setSelectedValue(e.target.value)}
+ *   label="Choose an option"
+ * />
+ */
+function DropdownInput({ value, options, onChange, label }: DropdownInputProp) {
   const [valueChanged, setValueChanged] = useState(false); // show lighter grey if not changed
   const [id] = useState(uniqueId());
   return (
-    <div className={label ? inputStyleWithLabel : inputStyleNoLabel}>
+    <div className={label ? textStyleWithLabel : textStyleNoLabel}>
       {label && <InputLabel label={label} id={id} />}
-      <input
+      <select
         id={id}
-        type="text"
         value={value}
         onChange={(e) => {
           onChange(e);
@@ -131,9 +246,15 @@ function DropdownInput({ value, onChange, label }: InputProp) {
             ? " text-penni-text-regular-light-mode"
             : " text-penni-text-tertiary-light-mode")
         }
-      />
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
 
-export { DropdownInput,ParagraphInput, PriceInput, TextInput };
+export { DropdownInput, ParagraphInput, PriceInput, TextInput };
