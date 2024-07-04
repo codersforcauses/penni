0546174b3f6af6d6b51b30c9074e7ed5cdbec21f
TroubledPhilip <troubledphilip@gmail.com>
2024-07-04 09:40:54 +0000

    Fix code style issues with Prettier

--- client/src/components/ui/TopNavtab.tsx
+++ client/src/components/ui/TopNavtab.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const TopNavtab = ()=>{
+const TopNavtab = () => {
   return (
     <div className="flex w-full border-b">
-            <button className="flex-1 py-2 px-4 text-gray-600 border-b-2 border-transparent hover:text-blue-500 focus:outline-none focus:text-blue-500 focus:border-blue-500">
-                Task Details
-            </button>
-            <button className="flex-1 py-2 px-4 text-gray-600 border-b-2 border-transparent hover:text-blue-500 focus:outline-none focus:text-blue-500 focus:border-blue-500">
-                Other Details
-            </button>
-        </div>
-  )
-}
+      <button className="flex-1 border-b-2 border-transparent px-4 py-2 text-gray-600 hover:text-blue-500 focus:border-blue-500 focus:text-blue-500 focus:outline-none">
+        Task Details
+      </button>
+      <button className="flex-1 border-b-2 border-transparent px-4 py-2 text-gray-600 hover:text-blue-500 focus:border-blue-500 focus:text-blue-500 focus:outline-none">
+        Other Details
+      </button>
+    </div>
+  );
+};
 
 export default TopNavtab;
