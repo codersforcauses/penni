4a48fb7a782ea467875e4d9bae9d2ba7c4649efe
keyman015 <25141441+keyman015@users.noreply.github.com>
2024-07-06 07:08:07 +0000

    Add documentation for main task details component

--- client/src/components/TaskDetails.tsx
+++ client/src/components/TaskDetails.tsx
@@ -50,6 +50,28 @@ const TaskDetailsSection = ({
   );
 };
 
+/**
+ * Task details component for a more in-depth explanation of a single task.
+ *
+ * @param props - The properties of the TaskDetails component.
+ * @param props.data - The details of a single task.
+ * @param props.emptyContentPlaceholder - (OPTIONAL) The replacement string for missing data in a field.
+ * @returns Task details component
+ *
+ * @example
+ * // Example Usage:
+ * const sampleTaskData = {
+ *   category: "Cleaning",
+ *   title: "Cleaning Up My House",
+ *   created_at: "10 Dec, 2022",
+ *   suburb: "Richmond",
+ *   state: "VIC",
+ *   estimated_time: "4 Hours",
+ *   budget: "$250",
+ *   description: "I need someone to help me clean my 2 bedroom apartment. I am moving out and I need to make sure it’s all clean.",
+ * };
+ * <TaskDetails data={sampleTaskData} />
+ */
 const TaskDetails = ({ data, emptyContentPlaceholder }: TaskDetailsProps) => {
   return (
     <div className="max-w-screen-sm overflow-hidden px-7">
