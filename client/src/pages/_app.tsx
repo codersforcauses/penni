1d459f70dc2a73f3c9d85031d9ee20452de4b59d
Martin <martinmitanoski@gmail.com>
2024-07-06 08:02:10 +0000

    Convert CSS to Tailwind

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,6 @@
 // _app.tsx
 import "@/styles/globals.css";
 import "@/styles/styles.css";
-import "@/styles/Card.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
