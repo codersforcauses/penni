06b8cc2a224bc4a6ce683e70c4dd5fbe47978a2a
TroubledPhilip <troubledphilip@gmail.com>
2024-07-06 08:04:17 +0000

    create the no-task page and the top bar component for it

--- client/src/components/ui/poster/create-task.tsx
+++ client/src/components/ui/poster/create-task.tsx
@@ -2,13 +2,22 @@ import { pages } from "next/dist/build/templates/app-page";
 import Image from "next/image";
 import React from "react";
 
+import { Button } from "../button";
 import TaskTopBar from "../task-top-bar";
 
 export default function CreateTask() {
   return (
-    <div className="justify-center">
+    <div className="flex flex-col items-center">
       <TaskTopBar />
-      <Image src="/task-blank-pages.svg" alt="No task" width={50} height={50} />
+      <Image
+        className="mt-52"
+        src="/task-blank-page.svg"
+        alt="No task"
+        width={200}
+        height={200}
+      />
+      <p className="mt-10 font-bold">You do not have any orders yet</p>
+      <Button className="mt-10 w-4/5" label="Create a Task" />
     </div>
   );
 }
