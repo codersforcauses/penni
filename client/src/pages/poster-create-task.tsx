251f9fc1da81f4b54cb0eea2d128a5f573db3eaa
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2024-07-06 14:15:04 +0800

    create taks page structure
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

--- client/src/pages/poster-create-task.tsx
+++ client/src/pages/poster-create-task.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import CreateTask from "@/components/ui/poster/create-task";
 import TaskTopBar from "@/components/ui/task-top-bar";
+import NewTask from "@/components/ui/poster/new-task";
+import TaskDetail from "@/components/ui/poster/task-detail";
 
 export default function PosterCreateTask() {
   return (
     <div>
       <CreateTask />
+      <NewTask />
     </div>
   );
 }
