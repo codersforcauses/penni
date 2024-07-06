d08af674a896fcf68fec7264be11cf9f1a06793b
keyman015 <25141441+keyman015@users.noreply.github.com>
2024-07-06 07:21:38 +0000

    Move the location of the onboarding component
    - Move the file to be in the main component folder.

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Inter as FontSans } from "next/font/google";
 import { useEffect, useState } from "react";
 
-import OnBoarding from "@/components/OnBoarding/OnBoarding";
+import OnBoarding from "@/components/OnBoarding";
 import { usePings } from "@/hooks/pings";
 import { cn } from "@/lib/utils";
 
