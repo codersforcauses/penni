4eee7aa52e1eaa00d67d367226bbfacb63105cfb
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2024-07-06 04:03:31 +0000

    init poster create task page file structure
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

---
+++ client/src/components/ui/bidder/bidder-detail.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BidderDetail() {
+  return <div>{/* BidderDetail here */}</div>;
+}
---
+++ client/src/components/ui/poster/new-task.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NewTask() {
+  return <div>{/* NewTask here */}</div>;
+}
