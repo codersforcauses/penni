f16d263b00885e12299d7d0cd8c9500cdabc7393
ErikaKK <491649804@qq.com>
2024-07-10 07:29:33 +0000

    Change state to optional

--- client/src/components/ui/task-card.tsx
+++ client/src/components/ui/task-card.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 // the props
 type TaskCardProps = {
-  state: "BIDDING" | "EXPIRED" | "ONGOING" | "COMPLETED";
+  state?: "BIDDING" | "EXPIRED" | "ONGOING" | "COMPLETED";
   category: string;
   title: string;
   date: string;
@@ -33,16 +33,18 @@ const TaskCard: React.FC<TaskCardProps> = ({
       className={`m-4 rounded-lg border p-4 ${state === "EXPIRED" ? "bg-gray-100 opacity-60" : "bg-white"} transition duration-300 ease-in-out`}
     >
       {/* the state (expried or bidding) */}
-      <div
-        className={`footnote inline-block rounded-lg px-3 py-1.5 font-medium ${state === "BIDDING" ? "bg-blue-100 text-blue-700" : state === "ONGOING" ? "bg-orange-100 text-orange-700" : state === "COMPLETED" ? "bg-green-100 text-green-700" : "bg-gray-300 text-gray-700"}`}
-      >
-        {state}
-      </div>
+      {state && (
+        <div
+          className={`footnote inline-block rounded-lg px-3 py-1.5 font-medium ${state === "BIDDING" ? "bg-blue-100 text-blue-700" : state === "ONGOING" ? "bg-orange-100 text-orange-700" : state === "COMPLETED" ? "bg-green-100 text-green-700" : "bg-gray-300 text-gray-700"}`}
+        >
+          {state}
+        </div>
+      )}
 
       {/* the category & title (in 1 column) */}
       <div className="mb-4 mt-2">
-        <h5 className="mb-1 mt-1 text-sm text-gray-500">{category}</h5>
-        <h3 className="text-lg font-semibold leading-tight">{title}</h3>
+        <h5 className="footnote mb-1 mt-1 text-gray-500">{category}</h5>
+        <h3 className="body-medium">{title}</h3>
       </div>
 
       {/* the date, location, duration & Price, price type (estimate or myoffer price)   (in 2 columns)*/}
@@ -58,7 +60,7 @@ const TaskCard: React.FC<TaskCardProps> = ({
                 objectFit="contain"
               />
             </div>
-            <p className="text-xs text-gray-500">{date}</p>
+            <p className="subheadline text-gray-500">{date}</p>
           </div>
           <div className="flex items-center space-x-2">
             <div className="relative h-4 w-4">
@@ -69,7 +71,7 @@ const TaskCard: React.FC<TaskCardProps> = ({
                 objectFit="contain"
               />
             </div>
-            <p className="text-xs text-gray-500">{location}</p>
+            <p className="subheadline text-gray-500">{location}</p>
           </div>
           <div className="flex items-center space-x-2">
             <div className="relative h-4 w-4">
@@ -80,17 +82,17 @@ const TaskCard: React.FC<TaskCardProps> = ({
                 objectFit="contain"
               />
             </div>
-            <p className="text-xs text-gray-500">{duration} hours</p>
+            <p className="subheadline text-gray-500">{duration} hours</p>
           </div>
         </div>
 
         {/* Column 2 */}
         <div className="flex flex-col space-y-2 text-right">
-          <p className="text-lg font-bold">
+          <p className="title3 font-bold">
             {/* show the different price based on the different price type */}$
             {priceType === "My Offer" ? myOfferPrice : estimatePrice}
           </p>
-          <p className="caption-semibold">{priceType}</p>
+          <p className="caption">{priceType}</p>
         </div>
       </div>
     </div>
