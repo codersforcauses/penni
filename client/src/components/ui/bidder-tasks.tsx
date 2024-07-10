f74990b5b5eb671accc494f9b56294565b6caaee
ErikaKK <491649804@qq.com>
2024-07-10 15:21:33 +0000

    Add bidder-tasks

---
+++ client/src/components/ui/bidder-tasks.tsx
@@ -0,0 +1,133 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+import BottomNav from "./bottom-nav";
+import { Button } from "./button";
+// import GetUserTasks from "./search";
+import TaskCard from "./task-card";
+import TopNavtab from "./top-navtab";
+
+interface SearchTaskProp {
+  userID?: number;
+  state?: string;
+}
+interface Task {
+  name: string;
+  title: string;
+  category: string;
+  date: string;
+  location: string;
+  duration: string;
+  estimatePrice: string;
+  myOfferPrice: string;
+  state: "BIDDING" | "EXPIRED" | "ONGOING" | "COMPLETED";
+  priceType: string;
+}
+
+async function GetUserTasks() {
+  const response = await fetch("/api/tasks-test");
+  if (!response.ok) {
+    throw new Error("Failed to fetch");
+  }
+  return response.json();
+}
+
+interface TaskListProps {
+  name: string;
+  state: string[];
+}
+
+const filterTasks = (tasks: Task[], name: string, states: string[]) => {
+  return tasks.filter(
+    (task) => task.name === name && states.includes(task.state),
+  );
+};
+
+const TaskList = ({ name, state }: TaskListProps) => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const res = await fetch("/api/tasks-test");
+        if (!res.ok) {
+          throw new Error("Failed to fetch");
+        }
+        const data = await res.json();
+        setTasks(data.tasks);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError(String(error));
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  const userTasks = filterTasks(tasks, name, state);
+  if (userTasks.length === 0) {
+    return (
+      <div className="relative top-36 flex flex-col items-center justify-center gap-5 px-4">
+        <Image
+          width={178}
+          height={171}
+          src="/no-task-img.svg"
+          alt="Image of a girl and flowers"
+        />
+        <p className="body">You do not have any tasks yet</p>
+        <Button className="headline h-14 w-full">Start Browsing</Button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {userTasks.map((task) => (
+          <TaskCard
+            title={task.title}
+            category={task.category}
+            date={task.date}
+            location={task.location}
+            duration={task.duration}
+            estimatePrice={task.estimatePrice}
+            myOfferPrice={task.myOfferPrice}
+            state={task.state}
+            priceType="My Offer"
+          />
+        ))}
+      </div>
+    );
+  }
+};
+
+export default function MyTasks(name: string) {
+  const myTasksStates = ["COMPLETED", "ONGOING"];
+  const myBidsStates = ["BIDDING", "EXPIRED"];
+
+  return (
+    <>
+      <TopNavtab
+        isFixed={true}
+        tabs={[
+          {
+            name: "My Tasks",
+            content: <TaskList name={name} state={myTasksStates} />,
+          },
+          {
+            name: "My Bids",
+            content: <TaskList name={name} state={myBidsStates} />,
+          },
+        ]}
+      />
+    </>
+  );
+}
--- client/src/components/ui/top-navtab.tsx
+++ client/src/components/ui/top-navtab.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface Tab {
   name: string;
@@ -7,14 +7,15 @@ interface Tab {
 
 interface TopNavtabProps {
   tabs: Tab[];
+  isFixed?: boolean;
 }
 
-export default function TopNavtab({ tabs }: TopNavtabProps) {
+export default function TopNavtab({ tabs, isFixed = false }: TopNavtabProps) {
   const [activeTab, setActiveTab] = useState(tabs[0].name);
 
   const handleTabClick = (tabName: string) => {
     setActiveTab(tabName);
-    console.log(`Active tab is now: ${tabName}`);
+    // console.log(`Active tab is now: ${tabName}`);
   };
 
   const buttonClass = (tabName: string) =>
@@ -23,10 +24,20 @@ export default function TopNavtab({ tabs }: TopNavtabProps) {
         ? "border-penni-main text-penni-main"
         : "border-transparent text-penni-grey-inactive hover:text-penni-main focus:border-penni-main focus:text-penni-main"
     }`;
+  const targetDivRef = useRef<HTMLDivElement>(null);
+  const [paddingTop, setPaddingTop] = useState<number>(0);
 
+  useEffect(() => {
+    if (targetDivRef.current) {
+      setPaddingTop(targetDivRef.current.offsetHeight);
+    }
+  }, []);
   return (
-    <div className="flex w-full flex-col">
-      <div className="flex border-b">
+    <div className="relative flex w-full flex-col">
+      <div
+        ref={targetDivRef}
+        className={`${isFixed ? "fixed top-0" : ""} z-40 flex w-full border-b bg-white`}
+      >
         {tabs.map((tab) => (
           <button
             key={tab.name}
@@ -37,7 +48,10 @@ export default function TopNavtab({ tabs }: TopNavtabProps) {
           </button>
         ))}
       </div>
-      <div className="p-4">
+      <div
+        className="relative"
+        style={{ paddingTop: `${isFixed ? paddingTop : 0}px` }}
+      >
         {tabs.find((tab) => tab.name === activeTab)?.content}
       </div>
     </div>
