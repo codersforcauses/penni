ad7b7a4bdbe2d489b979b22adf9c8d2eab270a22
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-07-11 10:55:18 +0000

    Add back button and modify styling on report page
    - Add back button in header that goes back to the previous page on the
      site using a router.
    - Modify the submit button to use correct variant styling instead of
      custom styling.
    - Adjust flex-box styling.

--- client/src/pages/account/report.tsx
+++ client/src/pages/account/report.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React, { FormEvent, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import WriteIn, { WriteInFormData } from "@/components/ui/write-in";
 
 export default function Report() {
+  const router = useRouter();
   const [formReady, setFormReady] = useState(false);
   const [formData, setFormData] = useState<WriteInFormData>({
     subject: "",
@@ -27,25 +29,34 @@ export default function Report() {
     }
   };
 
+  const handleBackClick = () => {
+    router.back();
+  };
+
   return (
     <div id="report" className="bg-penni-background-input-light-mode">
-      <div className="flex flex-row items-center justify-center bg-penni-background-light-mode">
-        {/* <Image src={} className="mr-auto"/> */}
-        <h2 className="body-medium p-[1.1rem] pt-6 text-center">
-          Report a problem
-        </h2>
+      <div className="flex flex-row items-center justify-center bg-penni-background-light-mode px-4 pb-2 pt-6">
+        <Button onClick={handleBackClick} variant="link">
+          <Image
+            src="/icons/arrow_back.svg"
+            alt="Back"
+            width={24}
+            height={24}
+          />
+        </Button>
+        <h2 className="body-medium flex-grow text-center">Report a problem</h2>
       </div>
       <div className="h-[0.12rem] bg-penni-background-input-light-mode pt-3"></div>
       <form onSubmit={handleSubmit}>
         <WriteIn imgUpload={true} onFormDataChange={handleFormDataChange} />
         <div className="h-0.5 bg-penni-background-input-light-mode pt-8"></div>
-        <div className="mt-4 flex items-center justify-center rounded-t-3xl bg-penni-background-light-mode pt-4 shadow-[rgba(0,0,0,0.05)_0px_-10px_15px_0px]">
+        <div className="mt-4 flex items-center justify-center rounded-t-3xl bg-penni-background-light-mode px-4 pt-4 shadow-[rgba(0,0,0,0.05)_0px_-10px_15px_0px]">
           <Button
             type="submit"
             className=""
             disabled={!formReady}
             variant="floating"
-            size="lg"
+            size="penni"
           >
             Submit report
           </Button>
