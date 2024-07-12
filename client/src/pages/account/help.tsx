845eba912bcf3482908942d31fd72f7012a17210
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-07-12 05:20:28 +0000

    Hot fix for compile errors and fix linking errors
    - Set the profile tag for report to send to the report page.
    - Modify the main function to match the desired style.
    - Fix imports by removing unused components.

--- client/src/pages/account/help.tsx
+++ client/src/pages/account/help.tsx
@@ -1,25 +1,23 @@
-import Link from "next/link";
 import React from "react";
 
 import Header from "@/components/ui/header";
 import { InfoIcon } from "@/components/ui/icons";
-import { ProfileTag } from "@/components/ui/profile_page_tags";
+import ProfileTag from "@/components/ui/profile_page_tags";
 
-const Help = () => {
+export default function Help() {
   return (
     <div className="flex min-h-screen flex-col bg-penni-background-input-light-mode">
       <Header title="Help centre" />
       <div className="h-[0.12rem] grow-0 bg-penni-background-input-light-mode pt-3"></div>
       <div className="flex flex-grow flex-col overflow-auto">
-        <Link href="/account/report">
-          <ProfileTag icon={InfoIcon} title="Report a problem" description="" />
-        </Link>
-        <Link href="/account/contact">
-          <ProfileTag icon={InfoIcon} title="Contact us" description="" />
-        </Link>
+        <ProfileTag
+          icon={InfoIcon}
+          title="Report a problem"
+          description=""
+          link="/account/report"
+        />
+        <ProfileTag icon={InfoIcon} title="Contact us" description="" />
       </div>
     </div>
   );
-};
-
-export default Help;
+}
