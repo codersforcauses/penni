c2fdf9e74c632ecdf49981c49c7531901db208a0
mouper <51354655+mouper@users.noreply.github.com>
2024-07-14 08:23:07 +0000

    Add PhoneIcon

--- client/src/pages/account/help.tsx
+++ client/src/pages/account/help.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "@/components/ui/header";
-import { InfoIcon } from "@/components/ui/icons";
+import { InfoIcon, PhoneIcon } from "@/components/ui/icons";
 import ProfileTag from "@/components/ui/profile_page_tags";
 
 export default function Help() {
@@ -9,8 +9,8 @@ export default function Help() {
     <div className="flex min-h-screen flex-col bg-penni-background-input-light-mode">
       <Header title="Help centre" />
       <div className="h-[0.12rem] grow-0 bg-penni-background-input-light-mode pt-3"></div>
-      <div className="flex flex-grow flex-col overflow-auto">
-        <ProfileTag icon={InfoIcon} title="Contact us" description="" />
+      <div className="-ml-2 flex flex-grow flex-col overflow-auto">
+        <ProfileTag icon={PhoneIcon} title="Contact us" description="" />
         <ProfileTag
           icon={InfoIcon}
           title="Report a problem"
