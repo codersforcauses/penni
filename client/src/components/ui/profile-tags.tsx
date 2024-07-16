247db53947ebed2bc6d059b9f29fe7a8d2fc98a0
Cameron O'Neill <25141441+keyman015@users.noreply.github.com>
2024-07-16 07:54:06 +0000

    Update icon importation to remain consistent

--- client/src/components/ui/profile-tags.tsx
+++ client/src/components/ui/profile-tags.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 
-import { ChevronRightIcon } from "./icons";
+import { ChevronRightIcon } from "@/components/ui/icons";
 
 interface ProfileTagProps {
   icon: string | React.ElementType;
