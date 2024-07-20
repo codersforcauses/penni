dff4cba5b9e533864eef129408b033e5d9c1da38
ErikaKK <491649804@qq.com>
2024-07-20 05:12:52 +0000

    Remove two unnecessary lines

--- server/api/settings.py
+++ server/api/settings.py
@@ -75,8 +75,6 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:3001",
-    "http://localhost:3001",
 ]
 
 ROOT_URLCONF = "api.urls"
