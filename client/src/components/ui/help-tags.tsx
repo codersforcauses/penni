6cec09f7f917bfacdd4fc18842bc58c659653f6b
mouper <51354655+mouper@users.noreply.github.com>
2024-07-20 12:40:46 +0800

    update styling add help-tags component

---
+++ client/src/components/ui/help-tags.tsx
@@ -0,0 +1,102 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+
+import { ChevronRightIcon } from "./icons";
+
+interface HelpTagProps {
+  icon: string | React.ElementType;
+  title: string;
+  description: string;
+  link?: string;
+  nestedContent?: React.ReactNode;
+}
+
+/**
+ * HelpTag component displays a Help tag with an icon, title, description, and optional nested content.
+ *
+ * @param {object} props - Props for the HelpTag component.
+ * @param {string | React.ElementType} props.icon - Icon to display. It can be a URL string or a React component.
+ * @param {string} props.title - Title of the Help tag.
+ * @param {string} props.description - Description of the Help tag.
+ * @param {string} [props.link] - Optional link URL. If provided, clicking the tag will navigate to this URL.
+ * @param {React.ReactNode} [props.nestedContent] - Optional nested content to display when the tag is clicked.
+ *
+ * @returns {React.FC<HelpTagProps>} The HelpTag component.
+ *
+ * @example
+ * // Usage with a URL icon and link
+ * <HelpTag
+ *   icon="/icons/user-icon.png"
+ *   title="Help"
+ *   description="View and edit your Help"
+ *   link="/Help"
+ * />
+ *
+ * @example
+ * // Usage with a React component icon and nested content
+ * import { LogoutIcon } from "./icons";
+ * <HelpTag
+ *   icon={LogoutIcon}
+ *   title="Logout"
+ *   description=""
+ *   nestedContent={<div>Logout content goes here</div>}
+ * />
+ */
+
+const HelpTag: React.FC<HelpTagProps> = ({
+  icon,
+  title,
+  description,
+  link,
+  nestedContent,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const IconComponent = typeof icon === "string" ? null : icon;
+
+  const handleClick = () => {
+    if (nestedContent) {
+      setIsOpen(!isOpen);
+    }
+  };
+
+  return (
+    <div className="relative flex flex-col bg-white">
+      <Link href={link || "#"} onClick={handleClick}>
+        <div
+          className="flex cursor-pointer items-center justify-between p-4 hover:bg-penni-text-regular-dark-mode"
+          style={{ height: "72px" }}
+        >
+          <div className="flex items-center">
+            <div className="ml-2 mr-6 h-6 w-6">
+              {IconComponent ? (
+                <IconComponent className="h-3 w-3" />
+              ) : (
+                <Image
+                  src={icon as string}
+                  alt={title}
+                  width={20}
+                  height={20}
+                />
+              )}
+            </div>
+            <div>
+              <h3 className="text-hb font-normal leading-hb text-penni-text-regular-light-mode">
+                {title}
+              </h3>
+              <p className="text-fn font-normal leading-fn text-penni-text-secondary-light-mode">
+                {description}
+              </p>
+            </div>
+          </div>
+          <div className="absolute right-5 top-1/2 -translate-y-1/2 transform">
+            <ChevronRightIcon className="h-3 w-3 text-penni-background-input-light-mode" />
+          </div>
+        </div>
+      </Link>
+      {nestedContent && isOpen && <div className="p-4">{nestedContent}</div>}
+    </div>
+  );
+};
+
+export default HelpTag;
--- client/src/pages/account/help.tsx
+++ client/src/pages/account/help.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 
 import Header from "@/components/ui/header";
+import HelpTag from "@/components/ui/help-tags";
 import { InfoIcon, PhoneIcon } from "@/components/ui/icons";
-import ProfileTag from "@/components/ui/profile-tags";
 
 export default function Help() {
   return (
     <div className="flex min-h-screen flex-col bg-penni-background-input-light-mode">
       <Header title="Help centre" />
       <div className="h-[0.12rem] grow-0 bg-penni-background-input-light-mode pt-3"></div>
-      <div className="-ml-2 flex flex-grow flex-col overflow-auto">
-        <ProfileTag icon={PhoneIcon} title="Contact us" description="" />
-        <ProfileTag
+      <div className="flex flex-grow flex-col overflow-auto">
+        <HelpTag icon={PhoneIcon} title="Contact us" description="" />
+        <div className="bg-penni-background-light-mode">
+          <hr className="mx-5 grow-0 bg-penni-text-regular-dark-mode" />
+        </div>
+        <HelpTag
           icon={InfoIcon}
           title="Report a problem"
           description=""
