fc775c1724df08f4d470fc4cb9d585928b4fa3f9
K1rk3y <133004197+K1rk3y@users.noreply.github.com>
2024-07-20 12:56:12 +0800

    Update serializers.py

--- server/app/serializers.py
+++ server/app/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from .models import Users, Profiles, Tasks, Bids, Payments
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
