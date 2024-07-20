593fb0374b1b3a300cd751ecdda80b4269f1eda3
SodaVolcano <23301921@student.uwa.edu.au>
2024-07-20 05:54:52 +0000

    finished styling

--- client/src/pages/poster/tasks/new-task.tsx
+++ client/src/pages/poster/tasks/new-task.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import { Button } from "@/components/ui/button";
 import { Form, FormData } from "@/components/ui/form";
+import Header from "@/components/ui/header";
 import {
   DropdownInput,
   ParagraphInput,
@@ -14,7 +15,6 @@ export default function NewTask() {
   function onSubmit(e: FormData) {
     console.log(e);
     router.push(`/poster/tasks/task-list`);
-    // api post data to backend.
     // TODO api post formData to backend. awiat navigate to my task list page.
     // router.push(`/poster/tasks/task-list`);
   }
@@ -22,27 +22,35 @@ export default function NewTask() {
 
   return (
     <>
-      <Form className="m-4" onSubmit={onSubmit}>
-        {/* Not using <Heading> Because of the vertical padding :( */}
-        <h1 className={headingStyle}>Task Information</h1>
-        <DropdownInput
-          label="Task Category"
-          options={["Cleaning", "Delivery", "Handyman", "Moving"]}
-        />
-        <SingleLineInput label="Task Title" type="text" />
-        <SingleLineInput label="Estimated Time" type="text" />
-        <SingleLineInput label="Estimated Price" type="price" />
-        <h1 className={headingStyle}>Date and Location</h1>
-        <SingleLineInput label="Select a date" type="date" />
-        <SingleLineInput label="Address" type="text" />
-        <DropdownInput
-          label="State"
-          options={["VIC", "NSW", "QLD", "NT", "WA", "SA", "TAS", "ACT"]}
-        />
-        <h1 className={headingStyle}>Task Details</h1>
-        <ParagraphInput label="" />
-        <Button label="Post Task" type="submit" />
-      </Form>
+      <div className="flex flex-col">
+        <Header title="New Task" className="sticky h-11 w-full" />
+        <Form className="w-full p-4" onSubmit={onSubmit}>
+          {/* Not using <Heading> Because of the vertical padding :( */}
+          <h1 className={headingStyle}>Task Information</h1>
+          <DropdownInput
+            label="Task Category"
+            options={["Cleaning", "Delivery", "Handyman", "Moving"]}
+          />
+          <SingleLineInput label="Task Title" type="text" />
+          <SingleLineInput label="Estimated Time" type="text" />
+          <SingleLineInput label="Estimated Price" type="price" />
+          <h1 className={headingStyle}>Date and Location</h1>
+          <SingleLineInput label="Select a date" type="date" />
+          <SingleLineInput label="Address" type="text" />
+          <DropdownInput
+            label="State"
+            options={["VIC", "NSW", "QLD", "NT", "WA", "SA", "TAS", "ACT"]}
+          />
+          <h1 className={headingStyle}>Task Details</h1>
+          <ParagraphInput label="" />
+          <Button
+            variant="default"
+            size="penni"
+            label="Post Task"
+            type="submit"
+          />
+        </Form>
+      </div>
     </>
   );
 }
