70f5542af26e6691e69e37bacdc02eeb5e2462f9
ErikaKK <491649804@qq.com>
2024-07-21 15:32:07 +0000

    Delete useless code

--- client/src/pages/poster/index.tsx
+++ client/src/pages/poster/index.tsx
@@ -6,35 +6,6 @@ import { axiosInstance } from "@/lib/api";
 
 import Create from "./tasks/create-task";
 import TaskList from "./tasks/task-list";
-// export default function PosterTasksPage() {
-//   const fetchUserData = async () => {
-//     try {
-//       const response = await axiosInstance.get("/app/tasks/");
-//       const jsonResponse = response.data;
-//       console.log(jsonResponse);
-//       const token = localStorage.getItem("token");
-//       const decoded = jwt.decode(token as string) as { user_id: string };
-//       const user_id = decoded.user_id;
-//       const findAttributeAndFilter = (obj: any, attributeName: string) => {
-//         let results: object[] = [];
-
-//         const search = (obj: any) => {
-//           results = obj.filter((item: any) => item[attributeName] == user_id);
-//         };
-//         search(obj);
-//         return results;
-//       };
-
-//       return console.log(findAttributeAndFilter(jsonResponse, "owner_id"));
-//     } catch (error) {
-//       console.error("Error fetching user data:", error);
-//     }
-//   };
-//   // fetchUserData();
-//   const tasks = fetchUserData();
-//   const hasTask = tasks.length > 0;
-//   return <div>{!hasTask ? <Create /> : <TaskList />}</div>;
-// }
 
 export default function PosterTasksPage() {
   const [tasks, setTasks] = useState<object[]>([]);
