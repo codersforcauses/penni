d04fbe1cfd14b1788b0a3078306f8455e81805cf
ErikaKK <491649804@qq.com>
2024-07-23 13:30:57 +0000

    Test api in bid-details.tsx. It works!

--- client/src/pages/poster/tasks/[taskid]/[bidderid]/bid-details.tsx
+++ client/src/pages/poster/tasks/[taskid]/[bidderid]/bid-details.tsx
@@ -11,25 +11,34 @@ export default function BidDetail() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string>("");
   const router = useRouter();
-  const task_id = router.query.taskid;
-  const bidder_id = router.query.bidderid;
+
   useEffect(() => {
     const fetchUserData = async () => {
+      const task_id = router.query.taskid;
+      const bidder_id = router.query.bidderid;
+      console.log(router.query);
+      // Ensure task_id and bidder_id is a string and not undefined
+      if (typeof task_id !== "string" || typeof bidder_id !== "string") {
+        console.error("waiting"); // the router id soooo slow!
+        return;
+      }
       try {
-        const response = await axiosInstance.get(`/app/tasks/${task_id}/bids/`); // Fetch bids for the task
+        const response = await axiosInstance.get(`/app/tasks/5/bids/`); // Fetch bids for the task_id:5. needs to replace to variable
         const jsonResponse = response.data;
-        const bidInfo = jsonResponse.filter(
-          (bid: any) => bid.bidder_id === bidder_id,
+        const bidInfo = jsonResponse.data.filter(
+          (bid: any) => bid.bidder_id === 6, // Fetch bid of the bidder_id:6. needs to replace to variable
         )[0];
         // Fetch profiles of all users
         const allProfiles = await axiosInstance.get(`/app/profiles/`);
+
         const bidderProfile = allProfiles.data.filter(
-          (profile: any) => profile.user_id === bidder_id,
+          (profile: any) => profile.user_id === 6,
         )[0];
-        bidInfo["avatar_url"] = bidderProfile.avatar_url;
+        console.log(bidderProfile);
+        // bidInfo["avatar_url"] = bidderProfile.avatar_url; // source of img may need change
         bidInfo["full_name"] = bidderProfile.full_name;
         bidInfo["bio"] = bidderProfile.bio;
-
+        console.log(bidInfo);
         setBid(bidInfo);
       } catch (error: unknown) {
         if (error instanceof Error) {
