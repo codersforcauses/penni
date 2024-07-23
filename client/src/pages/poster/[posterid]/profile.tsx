90a37112ad954a765f93af8bdfb85bdb0790f9ef
ErikaKK <491649804@qq.com>
2024-07-23 16:39:11 +0000

    Update api in profile.tsx

--- client/src/pages/poster/[posterid]/profile.tsx
+++ client/src/pages/poster/[posterid]/profile.tsx
@@ -2,12 +2,13 @@ import jwt from "jsonwebtoken";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
+import useFetchData from "@/hooks/use-fetch-data";
 import { axiosInstance } from "@/lib/api";
 
 import {
+  AboutInfoIcon,
   EditIcon,
   HelpCircleIcon,
-  InfoIcon,
   LogoutIcon,
   PaymentIcon,
   SettingsIcon,
@@ -16,47 +17,32 @@ import { PersonImg } from "../../../components/ui/person-detail";
 import ProfileTag from "../../../components/ui/profile-tags";
 
 const ProfilePage: React.FC = () => {
-  const [profile, setProfile] = useState<any>();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string>("");
   const router = useRouter();
-  const posterid = router.query.posterid;
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await axiosInstance.get(`/app/profiles/${posterid}/`);
-        const jsonResponse = response.data;
-        const token = localStorage.getItem("token");
-        if (!token) throw new Error("No token found");
-        const decoded = jwt.decode(token) as { email: string };
-        const user_email = decoded.email;
-        jsonResponse["email"] = user_email;
-        setProfile(jsonResponse);
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          setError(error.message);
-        } else {
-          setError("An unexpected error occurred");
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchUserData();
-  }, []);
+  const { posterid } = router.query;
+  const queryReady = typeof posterid === "string";
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-
+  const {
+    data: user,
+    loading: userLoading,
+    error: userError,
+  } = useFetchData(`/app/profiles/${posterid}/`, queryReady);
+  console.log(user);
+  if (userLoading) return <div>Loading...</div>;
+  if (userError) return <div>Error: {userError}</div>;
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("No token found");
+  const decoded = jwt.decode(token) as { email: string };
+  const email = decoded.email;
+  // img src needs to change to user.avatar_url later
   return (
     <div className="m-0">
       <div className="mt-20 flex flex-col items-center">
-        <PersonImg personImg={profile.avatar_url} size={120} />
+        <PersonImg personImg="" size={120} />
         <p className="mt-4 text-t3 font-semibold text-penni-text-regular-light-mode">
-          {profile.full_name}
+          {user.full_name}
         </p>
         <p className="text-sh font-normal text-penni-text-secondary-light-mode">
-          {profile.email}
+          {email}
         </p>
       </div>
       <div className="mt-6">
@@ -79,7 +65,7 @@ const ProfilePage: React.FC = () => {
           link=""
         />
         <ProfileTag
-          icon={InfoIcon}
+          icon={AboutInfoIcon}
           title="About"
           description="Lorem ipsum dolor sit amet."
           link=""
