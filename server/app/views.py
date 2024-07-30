6c9d9b015eab97f534c7b52264b359f5692437bd
Yunho Ding <gurumindc123@gmail.com>
2024-07-30 09:41:55 +0000

    chore: Remove unused code and refactor user creation logic in views.py

--- server/app/views.py
+++ server/app/views.py
@@ -1,4 +1,3 @@
-from rest_framework.decorators import action
 from .serializers import BidsSerializer
 from rest_framework import viewsets, status
 
@@ -43,32 +42,6 @@ class TasksViewSet(viewsets.ModelViewSet):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
-
-    # @action(detail=False, methods=['post'])
-    # def post_task(self, request):
-    #     serializer = self.get_serializer(data=request.data)
-    #     if serializer.is_valid():
-    #         task = serializer.save(status='open')
-    #         owner_data = UsersSerializer(task.user_id).data
-    #         response_data = {
-    #             'task_id': task.task_id,
-    #             'user': owner_data,
-    #             'status': 'success',
-    #             'message': 'Task created successfully.'
-    #         }
-    #         return Response(response_data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=st
-    # atus.HTTP_400_BAD_REQUEST)
-
-    # @action(detail=True, methods=['get'])
-    # def get_user_tasks(self, request, user_id=None):
-    #     if user_id:
-    #         tasks = Tasks.objects.filter(user_id=user_id)
-    #         serializer = TasksSerializer(tasks, many=True)
-    #         return Response({'status': 'success',
-    # 'data': serializer.data}, status=status.HTTP_200_OK)
-    #     return Response({'status': 'error', 'messa
-    # ge': 'User ID is required.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TaskLocationViewSet(viewsets.ModelViewSet):
@@ -135,57 +108,6 @@ class BidsViewSet(viewsets.ModelViewSet):
             }
         )
 
-    @action(detail=True, methods=["get"])
-    def get_task_bids(self, request, task_id=None):
-        if task_id:
-            bids = Bids.objects.filter(task_id=task_id)
-            serializer = BidsSerializer(bids, many=True)
-            return Response(
-                {"status": "success", "data": serializer.data},
-                status=status.HTTP_200_OK,
-            )
-        return Response(
-            {"status": "error", "message": "Task ID is required."},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-    @action(detail=True, methods=["post"])
-    def change_bid_status(self, request, pk=None):
-        valid_statuses = ["accepted", "rejected", "pending"]
-        status = request.data.get("status")
-
-        if status not in valid_statuses:
-            return Response(
-                {"status": "error", "message": "Invalid action type."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        bid = self.get_object()
-        bid.status = status
-        bid.save()
-
-        action_messages = {
-            "accepted": "Bid accepted.",
-            "rejected": "Bid rejected.",
-            "pending": "Bid pending.",
-        }
-
-        return Response({"status": "success", "message": action_messages[status]})
-
-    @action(detail=True, methods=["get"])
-    def get_user_bids(self, request, user_id=None):
-        if user_id:
-            bids = Bids.objects.filter(bidder_id=user_id)
-            serializer = BidsSerializer(bids, many=True)
-            return Response(
-                {"status": "success", "data": serializer.data},
-                status=status.HTTP_200_OK,
-            )
-        return Response(
-            {"status": "error", "message": "User ID is required."},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
 
 class RegistrationView(CreateAPIView):
     permission_classes = [AllowAny]
@@ -205,14 +127,3 @@ class RegistrationView(CreateAPIView):
         }
 
         return Response(response_data, status=status.HTTP_201_CREATED)
-
-
-# class ProfileViewSet(viewsets.ModelViewSet):
-#     queryset = Profiles.objects.all()
-#     serializer_class = ProfleSerializer
-#     parser_classes = (MultiPartParser, FormParser)
-#     permission_classes = [
-#         permissions.IsAuthenticatedOrReadOnly]
-
-#     def perform_create(self, serializer):
-#         serializer.save(author=self.request.user)
