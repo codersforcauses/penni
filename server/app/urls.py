707f688b2d7ed7c09750f63fd0868868503d5420
Yunho Ding <gurumindc123@gmail.com>
2024-07-30 09:30:17 +0000

    Remove commented out code unused in urls.py and models.py

--- server/app/urls.py
+++ server/app/urls.py
@@ -19,22 +19,6 @@ router.register(r"locations", TaskLocationViewSet, basename="locations")
 
 
 urlpatterns = [
-    # path("login/", obtain_jwt_token, name="get-jwt-token"),
-    # path("refresh/", refresh_jwt_token, name="refresh-jwt-token"),
-    # path("verify/", verify_jwt_token, name="verify-jwt-token"),
-    # get all bids by task
-    # path("tasks/<int:task_id>/bids/", BidsViewSet.as_view(
-    #     {'get': 'get_task_bids', 'post': 'create'}), name='bids-list'),
-    # # change bid status
-    # path("bids/<int:bid_id>/change_status", BidsViewSet.as_view(
-    #     {'post': 'change_bid_status'}), name='bids-change-status'),
-    # # Get tasks/bids by user
-    # # path("users/", ProfileViewSet.as_view({'get': 'list'}),
-    # name='users-list'),
-    # path("users/<int:user_id>/tasks",
-    #      TasksViewSet.as_view({'get': 'get_user_tasks'}), name='user-tasks'),
-    # path("users/<int:user_id>/bids",
-    #      BidsViewSet.as_view({'get': 'get_user_bids'}), name='user-bids'),
     path("", include(router.urls)),
     path("register/", RegistrationView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
